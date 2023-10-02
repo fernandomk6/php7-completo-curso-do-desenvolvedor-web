0e6575647dcd940f1165f5c403149cfa575dc2bd
fernando <fernandomk6@gmail.com>
2023-10-02 20:27:56 -0300

    Substituído arquivos da seção anterior

--- exercicio.php
+++ exercicio.php
@@ -13,7 +13,7 @@
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href=<?= "./{$_GET['dir']}/{$_GET['file']}.php" ?> 
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
             class="verde">Sem formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
