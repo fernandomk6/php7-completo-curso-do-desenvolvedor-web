83a6fcdf1249b5395490e6333b0df3238f31c084
fernandomk6 <fernandomk6@gmail.com>
2024-02-26 20:04:27 -0300

    iniciado desafio polindromo

--- funcoes/desafio-polindromo.php
+++ funcoes/desafio-polindromo.php
@@ -1,2 +1,16 @@
 <?php
 
+function is_polindrome ($word) {
+    $letters = explode("", $word);
+    $letters_reverse = [];
+    // 8, 
+    for ($i = count($letters) - 1; $i > 0; $i--) {
+        mb_split()
+        $letters_reverse = $letters[$i];
+    }
+
+    print_r($letters_reverse);
+}
+
+is_polindrome("fernando");
+
